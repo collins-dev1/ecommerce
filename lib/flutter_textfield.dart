0cf3c3c7c9d4be106f1d7710fd2ef175fc1ddb6b
OG-COLLINS/ <collinsxuccessful39@gmail.com>
2025-07-31 03:59:57 +0100

    flutter input

---
+++ lib/flutter_textfield.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  // runapp method
+  runApp(const CollinsEntryWidget());
+}
+
+// Entry Widget
+class CollinsEntryWidget extends StatelessWidget {
+  const CollinsEntryWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // return MaterialApp
+    return MaterialApp(
+      title: 'Collins Application',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: CollinsHomePage(),
+    );
+  }
+}
+
+// CollinsHomePage Widget
+class CollinsHomePage extends StatefulWidget {
+  const CollinsHomePage({super.key});
+
+  @override
+  State<CollinsHomePage> createState() => _CollinsHomePageState();
+}
+
+class _CollinsHomePageState extends State<CollinsHomePage> {
+  // Scaffold Key
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  // Username Controller
+  final TextEditingController _usernameController = TextEditingController();
+  // Password Controller
+  final TextEditingController _passwordController = TextEditingController();
+  // Text Are Controller
+  final TextEditingController _textAreaController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Collins App', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.black, // Set your desired background color here
+        elevation: 50,
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                icon: Icon(Icons.person, color: Colors.black),
+                // border: InputBorder.none,
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                ),
+                labelText: "User Name",
+                hintText: "Enter your name",
+              ),
+            ),
+          ),
+          // Password
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                icon: Icon(Icons.lock, color: Colors.black),
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                ),
+                labelText: "Password",
+                hintText: "Enter your password",
+              ),
+            ),
+          ),
+          // Text Area
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextField(
+              controller: _textAreaController,
+              maxLines: 5,
+              decoration: InputDecoration(
+                icon: Icon(Icons.text_fields, color: Colors.black),
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                ),
+                labelText: "Text Area",
+                hintText: "Enter your text here",
+              ),
+            ),
+          ),
+          // Login Button
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.blue, // Set the text color
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              ),
+              onPressed: () {
+                // Handle login action
+                String username = _usernameController.text;
+                String password = _passwordController.text;
+                String textArea = _textAreaController.text;
+                print(
+                  'Username: $username, Password: $password, Text Area: $textArea',
+                );
+              },
+              child: Text('Login'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
