ffd731a7e16a31b56e1f3f4968505443b8c38a16
OG-COLLINS/ <collinsxuccessful39@gmail.com>
2025-08-04 01:07:32 +0100

    card, loop and bottomnav

---
+++ lib/flutter_card.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  // runapp method
+  runApp(const CollinsEntryWidget());
+}
+
+// Entry Widget
+class CollinsEntryWidget extends StatelessWidget {
+  const CollinsEntryWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // return MaterialApp
+    return MaterialApp(
+      title: 'Collins Application',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: CollinsHomePage(),
+    );
+  }
+}
+
+// CollinsHomePage Widget
+class CollinsHomePage extends StatefulWidget {
+  const CollinsHomePage({super.key});
+
+  @override
+  State<CollinsHomePage> createState() => _CollinsHomePageState();
+}
+
+class _CollinsHomePageState extends State<CollinsHomePage> {
+  // Scaffold Key
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Collins App', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.black, // Set your desired background color here
+        elevation: 50,
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+      backgroundColor: Colors.yellow,
+      body: Center(
+        child: Container(
+          width: 300,
+          height: 200,
+          padding: EdgeInsets.all(10),
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            color: Colors.red,
+            elevation: 10,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ListTile(
+                  leading: Icon(Icons.album, color: Colors.white, size: 50),
+                  title: Text(
+                    'Davido 30B',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Best of Davido Music',
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                ),
+                // Button
+                ButtonBar(
+                  alignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.black, // Set the text color
+                      ),
+                      onPressed: () {
+                        print("playing");
+                      },
+                      child: Text("Play"),
+                    ),
+
+                    // pause
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.grey, // Set the text color
+                      ),
+                      onPressed: () {
+                        print("pausing");
+                      },
+                      child: Text("Pause"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,272 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  // runapp method
+  runApp(const CollinsEntryWidget());
+}
+
+// Entry Widget
+class CollinsEntryWidget extends StatelessWidget {
+  const CollinsEntryWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // return MaterialApp
+    return MaterialApp(
+      title: 'News Application',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          primary: Colors.white,
+          onPrimary: Colors.black,
+        ),
+      ),
+      home: CollinsHomePage(),
+    );
+  }
+}
+
+// CollinsHomePage Widget
+class CollinsHomePage extends StatefulWidget {
+  const CollinsHomePage({super.key});
+
+  @override
+  State<CollinsHomePage> createState() => _CollinsHomePageState();
+}
+
+class _CollinsHomePageState extends State<CollinsHomePage> {
+  // Scaffold Key
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  // add pageIndex
+  int pageIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'News App',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: () {
+              print("share");
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print("search");
+            },
+          ),
+          SizedBox(width: 10),
+        ],
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
+        elevation: 4.0, // Adds shadow
+        // ignore: deprecated_member_use
+        shadowColor: Colors.grey,
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Header Section
+            Container(
+              width: double.infinity,
+              height: 250,
+              // Background Image
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    "https://www.lindaikejisblog.com/photos/shares/aluko-wsl-2018-title_1754038632.webp",
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomRight,
+                    colors: [
+                      // ignore: deprecated_member_use
+                      Colors.black.withOpacity(1),
+                      // ignore: deprecated_member_use
+                      Colors.black.withOpacity(.0),
+                    ],
+                  ),
+                ),
+                padding: EdgeInsets.all(15),
+                child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                    "England more competitive and organized than Nigeria – Ex Chelsea star Aluko explains why she ditched Super Falcons",
+                    maxLines: 3,
+                    style: TextStyle(
+                      fontSize: 22,
+                      overflow: TextOverflow.ellipsis,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w900,
+                      height: 1.2,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            // Body Section
+            for (var i = 0; i < 10; i++)
+              CollinsBlogList().getCard(
+                thumbnail:
+                    "https://pagesix.com/wp-content/uploads/sites/3/2025/07/paris-jackson-copy-V2.jpg?quality=75&strip=all",
+                title:
+                    "Paris Jackson and her fiancé end their engagement a week after she was spotted crying in public",
+                description:
+                    "Paris Jackson, daughter of late pop icon Michael Jackson, and her fiancé Justin Long have split and ended their engagement.The breakup comes just over a week after the 27-year-old daug",
+                author: " Linda Ikeji",
+                date: "01-08-25",
+              ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: pageIndex,
+        onTap: (index) {
+          // rebuild ui
+          setState(() {
+            pageIndex = index;
+          });
+          // log
+          print("Index: $index");
+        },
+        selectedItemColor: Colors.red,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag),
+            label: "Ecommerce",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            label: "Cart",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// new class for blog list template
+class CollinsBlogList {
+  // Method
+  Widget getCard({thumbnail, title, description, author, date}) {
+    return Card(
+      margin: EdgeInsets.all(5),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(8, 12, 8, 12),
+        child: Row(
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(thumbnail),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Text
+                  Text(
+                    title,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    softWrap: true,
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w900,
+                      height: 1.2,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  // Date and Author
+                  Text.rich(
+                    TextSpan(
+                      text: "by",
+                      style: TextStyle(
+                        fontSize: 14,
+                        height: 1.2,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.grey,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: author,
+                          style: TextStyle(
+                            fontSize: 14,
+                            height: 1.2,
+                            fontWeight: FontWeight.w900,
+                            color: Colors.black,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              print("Linda Ikeji tapped");
+                            },
+                        ),
+                        TextSpan(
+                          text: " at $date",
+                          style: TextStyle(
+                            fontSize: 14,
+                            height: 1.2,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.black,
+                          ),
+                        ),
+                        // Comment
+                        TextSpan(
+                          text: " (2 Comment)",
+                          style: TextStyle(
+                            fontSize: 12,
+                            height: 1.2,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.grey,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              print("Comments tapped");
+                            },
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  SizedBox(height: 5),
+                  Text(
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    softWrap: true,
+                    description,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey,
+                      height: 1.2,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
